a55ec26ea45b27a16c9867789c2566714fd1e581
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-19 17:53:52 -0600

    156 - Link

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,15 +1,15 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, Link } from "react-router-dom";
 
 const NotFound = () => { 
 
   const error = useRouteError()
-  console.log(error);
 
   return (
     <div>
       <h1>{ error.status }</h1>
       <h3>{ error.statusText }</h3>
       <p>{ error.data }</p>
+      <Link to="/">Volver a Home</Link>
     </div>
   )
 }

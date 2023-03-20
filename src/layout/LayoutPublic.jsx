91d62f8fdcfd144c683a5242382203d426c67b89
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-19 21:32:19 -0600

    161 - Loading

--- src/layout/LayoutPublic.jsx
+++ src/layout/LayoutPublic.jsx
@@ -1,11 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 const LayoutPublic = () => { 
+
+  const navigation = useNavigation()
+
   return (
     <>
       <Navbar />
       <main className="container">
+        {navigation.state === 'loading' && (
+          <div className="alert alert-info my-5">Cargando...</div>
+        )}
         <Outlet />
       </main>
       <footer className="container text-center">El Footer</footer>

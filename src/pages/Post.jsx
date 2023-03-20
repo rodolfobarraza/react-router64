594a1edd907126047b93a02d443faaf1bafab994
RodolfoB <barraza.rodolfo@gmail.com>
2023-03-19 21:47:26 -0600

    162 - Error data

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -16,6 +16,12 @@ export default Post;
 
 export const loaderPost = async ({ params }) => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`)
+
+  if (!res.ok) throw ({
+    status: res.status,
+    statusText: 'Post no encontrado'
+  })
+
   const post = await res.json();
 
   return { post }
